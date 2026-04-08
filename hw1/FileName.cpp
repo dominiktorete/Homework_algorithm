ed9265a6c729d59019b056e837498a0eafcaa736
dominiktorete <emec85748@gmail.com>
2026-04-08 22:00:36 +0500

    Update FileName.cpp

--- hw1/FileName.cpp
+++ hw1/FileName.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
-int func(int arr[], int number, int size) {
-	int n = size / 2;
-	int top = size - 1;
-	while (true) {
-		if (number >= arr[size - 1])
-			return 0;
-		if (arr[n] <= number) {
-			n = (size + n) / 2;
+int binary_search(int arr[], int number, int size) {
+	int left = 0;
+	int right = size - 1;
+	if (arr[right] == number) return 0;
+	while (left < right - 1) {
+		int middle = (left + right) / 2;
+		if (arr[middle] <= number) {
+			left = middle;
 		}
-		else if (arr[n] > number) {
-			if (arr[n-1] <= number)return size - n;
-			top = n;
-			n = top / 2;
+		else {
+			right = middle;
 		}
 	}
-
+	return size - 1 - left;
 }
 
 int main() {
 
 	int arr[] = { 14, 16, 19, 32, 32, 32, 56, 69, 72 };
 	int size = sizeof(arr) / sizeof(arr[0]);
-
-	std::cout << "count element massive " << func(arr, 69, size);
-	
-
+	int point_ref{};
+	std::cout << "Enter point of reference: ";
+	std::cin >> point_ref;
+	std::cout << "Count element massive which is bigger than " << point_ref << ": " << binary_search(arr, point_ref, size);
 }
-
